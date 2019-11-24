c6b4a712584d185aa945e84fefb941ed1fdd6981
errysatrio <errysatriow@gmail.com>
2019-11-24 15:45:29 +0700

    exercise8

--- exercise7.js
+++ exercise7.js
@@ -4,16 +4,20 @@ for (let i = 0; i < rows1; i++) {
     console.log('*')
     }
     
+    
 console.log('nomor 2')
-var rows2=8
-let j=""
-for (let i =0; i < rows2; i++) {
-    j +='*'}
-console.log(j)
-for (let k = 0; k < rows2; k++) {
-    console.log(j)
-}
+var rows2 = 5
+let i = 0
+var hasil =''
 
+    for (let j = 0; j < rows2; j++) {
+        console.log(hasil)
+       while (i<rows2) {
+          i++
+          hasil+='*'
+    }
+}
+console.log(hasil)
 
 
 console.log('nomor 3')
@@ -21,8 +25,8 @@ var rows3=5
 j=''
 for (let k = 0; k < rows3; k++) {
     k +='*'
-    console.log("")
-for (let i =0; i < rows3; i++) {
-    j +='*'
-console.log(j)}
+    for (let i =0; i < rows3; i++) {
+        j +='*'
+        console.log(j)
+    }
 }
